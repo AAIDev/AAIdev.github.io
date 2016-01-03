1d974f49ec6bc7f0d2272376c5c1f522d00543b2
AAIDEV <awbkerbal@gmail.com>
2016-01-03 14:41:48 -0800

    Update script.js

--- newwebsite/script.js
+++ newwebsite/script.js
@@ -4,14 +4,20 @@ Main Script
 //Hide and seek!
 var p1 = document.getElementByID(p1);
 function to1() {
-  document.getElementById("p1").innerHTML = 'Whether you came for the great free arduino projects, the libraries, the generators, or the pizza.... <span style="font-weight: bold;">WAIT A MINUTE- WE DONT HAVE PIZZA</span>';
+  document.getElementById("main").innerHTML = '<p id="p1">Whether you came for the great free arduino projects, the libraries, the generators, or the pizza.... <span style="font-weight: bold;">WAIT A MINUTE- WE DONT HAVE PIZZA</span></p>';
   document.getElementById("members").className = "selected";
   document.getElementById("contact").className = "";
   document.getElementById("projects").className = "";
 }
 function to2() {
-  document.getElementById("p1").innerHTML = "Welcome to the homepage of the most innovative developers in the galaxy.";
+  document.getElementById("main").innerHTML = '<p id="p1">Welcome to the homepage of the most innovative developers in the galaxy.</p>';
   document.getElementById("projects").className = "selected";
   document.getElementById("members").className = "";
   document.getElementById("contact").className = "";
+}
+function to3() {
+  document.getElementById("main").innerHTML = '<p id="p1">Welcome to the homepage of the most innovative developers in the galaxy.</p>';
+  document.getElementById("projects").className = "";
+  document.getElementById("members").className = "";
+  document.getElementById("contact").className = "selected";
 }
