dc1de69980e5353bb33a58a6022f1c77e00a4858
AAIDEV <awbkerbal@gmail.com>
2016-01-03 14:31:54 -0800

    Update script.js

--- newwebsite/script.js
+++ newwebsite/script.js
@@ -1,1 +1,13 @@
-
+/*
+Main Script
+*/
+//Hide and seek!
+var p1 = document.getElementByID(p1);
+function to1() {
+  document.getElementById("p1").innerHTML = "Whether you came for the great free arduino projects, the libraries, the generators, or the pizza.... <span style="font-weight: bold;">WAIT A MINUTE- WE DONT HAVE PIZZA</span>";
+  document.getElementById("members").className = "selected";
+}
+function to2() {
+  document.getElementById("p1").innerHTML = "Welcome to the homepage of the most innovative developers in the galaxy.";
+  document.getElementById("projects").className = "selected";
+}
